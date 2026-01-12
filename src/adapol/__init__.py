f8cc355f8bda18c3e53ab40fb54681c6f24b2e44
Hugo U. R. Strand <hugo.strand@gmail.com>
2026-01-12 21:01:33 +0100

    [init] drop explicit imports in __init__.py

--- src/adapol/__init__.py
+++ src/adapol/__init__.py
@@ -1,4 +1,1 @@
-from .hybfit import hybfit, hybfit_triqs
-from .anacont import anacont
-from .fit_utils import check_psd
 __all__ = ["hybfit", "hybfit_triqs", "check_psd", "anacont", "anacont_triqs"]
